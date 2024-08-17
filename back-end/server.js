b8cfea23ed5eea38e72d8e30e2bb643b5b5ac7ca
Sujit Kar <106083603+SujitKar1195@users.noreply.github.com>
2024-08-17 10:37:30 +0530

    Update server.js
    changes in cors

--- back-end/server.js
+++ back-end/server.js
@@ -11,7 +11,7 @@ import cartRouter from './routes/cart.route.js';
 import orderRouter from './routes/orders.route.js';
 
 const corsOptions = {
-  origin: 'https://litwave.netlify.app',
+  origin: 'https://litwave.vercel.app',
   optionsSuccessStatus: 200,
 };
 
